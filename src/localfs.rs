701896973075a42244a43bc3df7a43894fdff35c
John Carmack <johncarmack@me.com>
2023-06-30 09:55:45 -0700

    Add local size check

--- src/localfs.rs
+++ src/localfs.rs
@@ -1,6 +1,6 @@
-use std::{fs, path::Path};
-
+use crate::db;
 use sedregex::find_and_replace;
+use std::{fs, path::Path};
 
 pub fn create_download_folder(dropbox_path: &str, local_path: &str) -> () {
     let base_name = Path::new(&dropbox_path)
@@ -13,5 +13,24 @@ pub fn create_download_folder(dropbox_path: &str, local_path: &str) -> () {
         .to_string();
     if !std::path::Path::new(&local_dir).exists() {
         fs::create_dir_all(&local_dir).unwrap()
+    }
+}
+
+pub fn get_local_size(local_path: &str) -> i64 {
+    let path = Path::new(local_path);
+    let file_size = std::fs::metadata(path).expect("it exists I swear").len();
+    file_size.try_into().unwrap()
+}
+
+pub fn confirm_local_size(
+    connection: &sqlite::ConnectionWithFullMutex,
+    dropbox_path: &str,
+    local_path: &str,
+) {
+    let dropbox_size = db::get_dropbox_size(&connection, &dropbox_path);
+    let local_size = get_local_size(&local_path);
+    match dropbox_size == local_size {
+        true => (),
+        false => panic!("🚫  Local file size {local_size} does not match DropBox {dropbox_size}"),
     }
 }
