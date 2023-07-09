d47f759a360bcba3823d46804e5835eedccc2baf
John Carmack <johncarmack@me.com>
2023-07-09 15:25:30 -0700

    Replace std::env with custom getenv function

--- src/progress.rs
+++ src/progress.rs
@@ -1,12 +1,12 @@
+use crate::util::getenv;
 use indicatif;
-use std::env;
 
 pub type Progress = indicatif::ProgressBar;
 pub type MultiProgress = indicatif::MultiProgress;
 pub type ProgressStyle = indicatif::ProgressStyle;
 
 pub fn new_multi_progress() -> MultiProgress {
-    let silent: bool = env::var("SILENT").unwrap() == "true";
+    let silent: bool = getenv("SILENT").unwrap() == "true";
     let m = MultiProgress::new();
     if silent {
         m.set_draw_target(indicatif::ProgressDrawTarget::hidden());
@@ -17,7 +17,7 @@ pub fn new_multi_progress() -> MultiProgress {
 
 pub fn new(total: u64, style: &str) -> Progress {
     let mut pb = Progress::new(total);
-    let silent: bool = env::var("SILENT").unwrap() == "true";
+    let silent: bool = getenv("SILENT").unwrap() == "true";
     if silent {
         pb.set_draw_target(indicatif::ProgressDrawTarget::hidden());
         return pb;
